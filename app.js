b069ac6f9ade19739e6cd74c93f3e519256b7ce6
Loki17 <Tkl33pac@proton.me>
2024-06-19 08:15:15 +0100

    start swiper

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 import * as apiFunctions from "./js/apiFunctions.js";
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 19,
+  navigation: {
+    nextEl: ".swiper-arrow-next",
+    prevEl: ".swiper-arrow-prev",
+  },
+});
