8ced35221953dbca40a986eabd06ba8b7e5e621b
Loki17 <Tkl33pac@proton.me>
2024-06-19 13:40:48 +0100

    swiper + responsive

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 import * as apiFunctions from "./js/apiFunctions.js";
-
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 19,
-  navigation: {
-    nextEl: ".swiper-arrow-next",
-    prevEl: ".swiper-arrow-prev",
-  },
-});
---
+++ js/swiperVariables.js
@@ -0,0 +1,43 @@
+const myBreakpoints = {
+  1280: {
+    slidesPerView: 4,
+    spaceBetween: 19,
+  },
+  1024: {
+    slidesPerView: 3,
+  },
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 10,
+  },
+};
+
+var swiperResult = new Swiper(".swiper-result", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".result-arrow-next",
+    prevEl: ".result-arrow-prev",
+  },
+  breakpoints: myBreakpoints,
+});
+
+var swiperReleases = new Swiper(".swiper-latest-releases", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".releases-arrow-next",
+    prevEl: ".releases-arrow-prev",
+  },
+  breakpoints: myBreakpoints,
+});
+
+var swiperMoviesBygenre = new Swiper(".swiper-movies-by-genre", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".movies-by-genre-arrow-next",
+    prevEl: ".movies-by-genre-arrow-prev",
+  },
+  breakpoints: myBreakpoints,
+});
