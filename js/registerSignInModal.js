86cf5c9b5806900b70f6d8abfe6f763cd3f2c94a
Loki17 <Tkl33pac@proton.me>
2024-06-21 15:24:25 +0100

    signinup form js

--- js/registerSignInModal.js
+++ js/registerSignInModal.js
@@ -1,54 +1,122 @@
 const loginForm = `
-        <form id="loginForm" class="login-signUp-Form">
           <input type="text" placeholder="username" id="usernameLogin" />
           <input type="password" placeholder="password" id="passwordLogin" />
           <div class="remember-me">
             <input type="checkbox" id="rememberMeLogin" />
             <label for="rememberMeLogin">Remember me</label>
           </div>
-          <button>Login</button>
-        </form>`;
+          <button>Login</button>`;
 
 const signUpForm = `
-        <form id="SignUpForm" class="login-signUp-Form">
           <input type="text" placeholder="username" id="usernameSignUP" />
           <input type="password" placeholder="password" id="passwordSignUP" />
           <input type="text" placeholder="email" id="emailSignUP" />
-          <button>SignUp</button>
-        </form>`;
+          <button>SignUp</button>`;
 
 const signInBtn = document.querySelectorAll(".signInBtn");
 const registerBtn = document.querySelectorAll(".registerBtn");
 const signFormModal = document.getElementById("signForm");
 const loginsignUpForm = document.querySelector(".login-signUp-Form");
 const forgotPassword = document.querySelector(".forgot-password");
+const notMember = document.querySelector(".not-member");
 const signupBtn = document.querySelector(".SignupBtn");
 const loginBtn = document.querySelector(".LoginBtn");
 const crossSign = signFormModal.querySelector(".cross");
 
 crossSign.addEventListener("click", () => {
+  document.querySelector("body").classList.remove("no-scroll");
   signFormModal.style.display = "none";
   signFormModal.close();
 });
 
+signFormModal.addEventListener("click", (e) => {
+  const rect = signFormModal.getBoundingClientRect();
+  if (
+    e.clientX < rect.left ||
+    e.clientX > rect.right ||
+    e.clientY < rect.top ||
+    e.clientY > rect.bottom
+  ) {
+    document.querySelector("body").classList.remove("no-scroll");
+    signFormModal.style.display = "none";
+    signFormModal.close();
+  }
+});
+
+const displayLoginForm = () => {
+  loginsignUpForm.innerHTML = loginForm;
+  forgotPassword.style.display = "block";
+  notMember.style.display = "block";
+  signupBtn.classList.remove("active-red");
+  loginBtn.classList.add("active-red");
+  const signupBtns = document.querySelectorAll(".SignupBtnM");
+  signupBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      displaySignupForm();
+    });
+  });
+  signFormModal.removeEventListener("submit", signFormForSignUp);
+  signFormModal.addEventListener("submit", signFormForLogin);
+};
+
+const displaySignupForm = () => {
+  loginsignUpForm.innerHTML = signUpForm;
+  forgotPassword.style.display = "none";
+  notMember.style.display = "none";
+  signupBtn.classList.add("active-red");
+  loginBtn.classList.remove("active-red");
+  const loginBtns = document.querySelector(".LoginBtnM");
+  loginBtns.addEventListener("click", () => {
+    displayLoginForm();
+  });
+  signFormModal.removeEventListener("submit", signFormForLogin);
+  signFormModal.addEventListener("submit", signFormForSignUp);
+};
+
 signInBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    loginsignUpForm.innerHTML = loginForm;
+    displayLoginForm();
     signFormModal.style.display = "flex";
     signFormModal.showModal();
-    forgotPassword.style.display = "block";
-    signupBtn.classList.remove("active-red");
-    loginBtn.classList.add("active-red");
+    document.querySelector("body").classList.add("no-scroll");
   });
 });
 
 registerBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    loginsignUpForm.innerHTML = signUpForm;
+    displaySignupForm();
     signFormModal.style.display = "flex";
     signFormModal.showModal();
-    forgotPassword.style.display = "none";
-    signupBtn.classList.add("active-red");
-    loginBtn.classList.remove("active-red");
+    document.querySelector("body").classList.add("no-scroll");
   });
 });
+
+// modif pour + tard si jms
+const signFormForSignUp = (e) => {
+  e.preventDefault();
+  let inputs = document.querySelectorAll(".login-signUp-Form input");
+  let username = document.getElementById("usernameSignUP");
+  let password = document.getElementById("passwordSignUP");
+  let email = document.getElementById("emailSignUP");
+  const regex = /^[\w\d._%+-]+@(?:[\w\d-]+\.)+(\w{2,})(,|$)/;
+  if (!username.value) {
+    username.style.border = "1px solid red";
+  } else if (!password.value) {
+    password.style.border = "1px solid red";
+  } else if (!email.value.match(regex)) {
+    email.style.border = "1px solid red";
+  } else {
+    inputs.forEach((i) => {
+      i.style.border = "2px solid green";
+    });
+    console.log(username.value, password.value, email.value);
+  }
+};
+
+const signFormForLogin = (e) => {
+  e.preventDefault();
+  let username = document.getElementById("usernameLogin").value;
+  let password = document.getElementById("passwordLogin").value;
+  let checkbox = document.getElementById("rememberMeLogin").checked;
+  console.log(password, username, checkbox);
+};
